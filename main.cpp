663a10be318a9f838b0224132196af2be0aae19d
yang <yangcheng@zuzuche.com>
2024-02-28 18:21:39 +0800

    调通sherpa-onnx tts.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 //#include "yolov5-lite/yolov5_lite.cpp"
-#include "yolo-nas/yolo_nas.cpp"
+//#include "yolo-nas/yolo_nas.cpp"
+#include "sherpa-onnx-tts/sherpa_tts.cpp"
 
 int main() {
 
 //    test_yolov5_lite();
-    test_yolo_nas();
+//    test_yolo_nas();
+    test_sherpa_tts();
 
     return 0;
 }
--- sherpa-onnx-tts/sherpa_tts.cpp
+++ sherpa-onnx-tts/sherpa_tts.cpp
@@ -3,90 +3,47 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "cargs.h"
 #include "sherpa-onnx/c-api/c-api.h"
-
-static struct cag_option options[] = {
-        {.identifier = 'h',
-                .access_letters = "h",
-                .access_name = "help",
-                .description = "Show help"},
-        {.access_name = "vits-model",
-                .value_name = "/path/to/xxx.onnx",
-                .identifier = '0',
-                .description = "Path to VITS model"},
-        {.access_name = "vits-lexicon",
-                .value_name = "/path/to/lexicon.txt",
-                .identifier = '1',
-                .description = "Path to lexicon.txt for VITS models"},
-        {.access_name = "vits-tokens",
-                .value_name = "/path/to/tokens.txt",
-                .identifier = '2',
-                .description = "Path to tokens.txt for VITS models"},
-        {.access_name = "vits-noise-scale",
-                .value_name = "0.667",
-                .identifier = '3',
-                .description = "noise_scale for VITS models"},
-        {.access_name = "vits-noise-scale-w",
-                .value_name = "0.8",
-                .identifier = '4',
-                .description = "noise_scale_w for VITS models"},
-        {.access_name = "vits-length-scale",
-                .value_name = "1.0",
-                .identifier = '5',
-                .description =
-                "length_scale for VITS models. Default to 1. You can tune it "
-                "to change the speech speed. small -> faster; large -> slower. "},
-        {.access_name = "num-threads",
-                .value_name = "1",
-                .identifier = '6',
-                .description = "Number of threads"},
-        {.access_name = "provider",
-                .value_name = "cpu",
-                .identifier = '7',
-                .description = "Provider: cpu (default), cuda, coreml"},
-        {.access_name = "debug",
-                .value_name = "0",
-                .identifier = '8',
-                .description = "1 to show debug messages while loading the model"},
-        {.access_name = "sid",
-                .value_name = "0",
-                .identifier = '9',
-                .description = "Speaker ID. Default to 0. Note it is not used for "
-                               "single-speaker models."},
-        {.access_name = "output-filename",
-                .value_name = "./generated.wav",
-                .identifier = 'a',
-                .description =
-                "Filename to save the generated audio. Default to ./generated.wav"},
-
-        {.access_name = "tts-rule-fsts",
-                .value_name = "/path/to/rule.fst",
-                .identifier = 'b',
-                .description = "It not empty, it contains a list of rule FST filenames."
-                               "Multiple filenames are separated by a comma and they are "
-                               "applied from left to right. An example value: "
-                               "rule1.fst,rule2,fst,rule3.fst"},
-
-        {.access_name = "max-num-sentences",
-                .value_name = "2",
-                .identifier = 'c',
-                .description = "Maximum number of sentences that we process at a time. "
-                               "This is to avoid OOM for very long input text. "
-                               "If you set it to -1, then we process all sentences in a "
-                               "single batch."},
-
-        {.access_name = "vits-data-dir",
-                .value_name = "/path/to/espeak-ng-data",
-                .identifier = 'd',
-                .description =
-                "Path to espeak-ng-data. If it is given, --vits-lexicon is ignored"},
-
-};
 
 
 
 int test_sherpa_tts(){
 
-    return 1;
+    SherpaOnnxOfflineTtsConfig config;
+    memset(&config, 0, sizeof(config));
+    config.model.vits.model = "/Users/yang/CLionProjects/test_onnxruntime/sherpa-onnx-tts/vits-aishell3.int8.onnx";
+    config.model.vits.lexicon = "/Users/yang/CLionProjects/test_onnxruntime/sherpa-onnx-tts/lexicon.txt";
+    config.model.vits.tokens = "/Users/yang/CLionProjects/test_onnxruntime/sherpa-onnx-tts/tokens.txt";
+    config.rule_fsts = "/Users/yang/CLionProjects/test_onnxruntime/sherpa-onnx-tts/rule.fst";
+    config.model.vits.noise_scale = 0.667;
+    config.model.vits.noise_scale_w = 0.8;
+    config.model.vits.length_scale = 1.0;
+    config.model.num_threads = 1;
+    config.model.provider = "cpu";
+    config.model.debug = 0;
+    config.max_num_sentences = 2;
+
+    int32_t sid = 0;
+    const char *filename = strdup("/Users/yang/CLionProjects/test_onnxruntime/sherpa-onnx-tts/generated.wav");
+    const char *text = "床前明月光，疑是地上霜。举头望明月，低头思故乡。";
+
+    // 加载配置
+    SherpaOnnxOfflineTts *tts = SherpaOnnxCreateOfflineTts(&config);
+    // 生成音频
+    const SherpaOnnxGeneratedAudio *audio =
+            SherpaOnnxOfflineTtsGenerate(tts, text, sid, 1.0);
+    // 保存文件
+    SherpaOnnxWriteWave(audio->samples, audio->n, audio->sample_rate, filename);
+    // 销毁模型
+    SherpaOnnxDestroyOfflineTtsGeneratedAudio(audio);
+    SherpaOnnxDestroyOfflineTts(tts);
+
+//    fprintf(stderr, "Input text is: %s\n", text);
+//    fprintf(stderr, "Speaker ID is is: %d\n", sid);
+//    fprintf(stderr, "Saved to: %s\n", filename);
+
+//    free((void *)filename);
+
+    return 0;
+//    return 1;
 }
