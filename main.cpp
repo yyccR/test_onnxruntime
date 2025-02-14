985b1f162a6784f2b1bfa1d842a57d9626d45b03
yang <yangcheng@zuzuche.com>
2025-02-14 20:24:07 +0800

    add silero_vad_v5

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 //#include "yolov5-lite/yolov5_lite.cpp"
 //#include "yolo-nas/yolo_nas.cpp"
-#include "sherpa-onnx-tts/sherpa_tts.cpp"
+//#include "sherpa-onnx-tts/sherpa_tts.cpp"
+#include "silero-vad/silero_vad_v5.cpp"
 
 int main() {
 
 //    test_yolov5_lite();
 //    test_yolo_nas();
-    test_sherpa_tts();
+//    test_sherpa_tts();
 
+    test_silero_vad_v5();
     return 0;
 }
